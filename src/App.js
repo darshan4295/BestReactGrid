6ec855af49a1e882173e0b090610d486c6633008
Darshan Hande <dhande43@gmail.com>
2023-12-28 18:23:55 +0530

    wqwqw

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BestReactGrid, Column } from '@sencha/best-react-grid';
+import '@sencha/best-react-grid/dist/themes/grui.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    const renderColumn = function () {
+      return (
+        <img
+          style={{ width: 100, height: 100 }}
+          src="https://i.dummyjson.com/data/products/19/thumbnail.jpg"
+        ></img>
+      );
+    };
+
+    return (
+      <BestReactGrid
+        infinite={true}
+        scrollable={true}
+        variableHeights={true}
+        userSelectable={true}
+        key="viewQuery"
+        autoWidth={false}
+        store={{
+          type: 'virtual',
+          fields: [
+            'firstName',
+            'lastName',
+            'address',
+            'company',
+            'title',
+            {
+              name: 'id',
+              type: 'int',
+            },
+          ],
+          loadData: async function (params) {
+            const response = await fetch(
+              'https://llbzr8dkzl.execute-api.us-east-1.amazonaws.com/production/user?' +
+                new URLSearchParams(params)
+            );
+            const jsonResponse = await response.json();
+            const dataArray = await jsonResponse;
+
+            return dataArray;
+          },
+          rootProperty: 'users',
+          remoteSort: true,
+          countProperty: 'totalCount',
+          pageSize: 25,
+          autoLoad: true,
+        }}
+        style={{ width: '500px', height: '700px' }}
+        columnLines
+      >
+        <Column
+          field="firstName"
+          text="Column 1"
+          flex="1"
+          renderer={renderColumn}
+        />
+        <Column field="lastName" text="Column 2" flex="1" />
+        <Column field="company" text="Column 3" flex="1" />
+      </BestReactGrid>
+    );
+  }
 }
-
-export default App;
